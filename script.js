476bc30210e7bdc21c10c3bbc74638f2f6bfa5da
LSL-Coding51 <168805232+LSL-Coding51@users.noreply.github.com>
2024-07-13 21:03:24 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const input = document.getElementById("search-input");
-    const output = document.getElementById("output");
-
-    input.addEventListener("input", function () {
-        const text = input.value;
-        const regex = new RegExp(`(${text.split('').join('|')})`, 'gi');
-        
-        if (text.length > 0) {
-            output.innerHTML = text.replace(regex, match => `<span class="highlight">${match}</span>`);
-        } else {
-            output.innerHTML = input.value;
-        }
-    });
-});
+function back(){
+    history.go(-1)
+}
